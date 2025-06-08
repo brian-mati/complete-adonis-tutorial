a20e5cecf01f355154b1aa4eba424eea38b303b5
brian-mati <brianmuthomimati@gmail.com>
2025-06-08 16:03:58 +0300

    changed full name to user name

--- app/models/user.ts
+++ app/models/user.ts
@@ -14,7 +14,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
   declare id: number
 
   @column()
-  declare fullName: string | null
+  declare userName: string | null
 
   @column()
   declare email: string
