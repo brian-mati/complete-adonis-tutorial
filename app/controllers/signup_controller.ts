8b08ad6fdab5257b1551206e66dbcd1a8d8436c8
brian-mati <brianmuthomimati@gmail.com>
2025-06-08 16:53:09 +0300

    added signup  route router.ts

--- app/controllers/signup_controller.ts
+++ app/controllers/signup_controller.ts
@@ -1,11 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class AuthController {
-  async signup({}: HttpContext) {}
+  async view({}: HttpContext) {}
 
   async store({}: HttpContext) {}
-
-  async update({}: HttpContext) {}
-
-  async delete({}: HttpContext) {}
 }
--- start/routes.ts
+++ start/routes.ts
@@ -9,10 +9,11 @@
 
 import router from '@adonisjs/core/services/router'
 
-const AuthController = () => import('#controllers/signup_controller')
+const SignupController = () => import('#controllers/signup_controller')
 
 router.on('/').render('pages/home')
 
 router.group(() => {
-  router.get('signup', [AuthController, 'view'])
+  router.get('signup', [SignupController, 'view'])
+  router.post('signup', [SignupController, 'store'])
 })
